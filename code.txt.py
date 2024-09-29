8d76a6c1197bdf7117384e9a3d08d76f6f18c6f5
izabella <148817915+izabellatec@users.noreply.github.com>
2024-09-29 16:54:31 +0000

    modificaçoes 1

---
+++ code.txt.py
@@ -0,0 +1,4 @@
+string = input("digite uma string: ")
+numero = int(input("digite um numero inteiro: ")) 
+
+print( (string + ' ')* numero)
---
+++ open.mat.py
@@ -0,0 +1,15 @@
+num1= int(input("digite o primeiro numero: "))
+num2= int(input("digite o segundo numero: "))
+
+operacao = input("digite a operaçao que deseja realizar (+, -,  *, /,): ")
+
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-': 
+    print(num1 + num2)
+elif operacao == '*': 
+    print(num1 + num2) 
+elif operacao == '/': 
+    print(num1 + num2)   
+else:
+   print("operacao invalida")
